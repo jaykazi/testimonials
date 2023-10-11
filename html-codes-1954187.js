53de65fbd936aa40163e0e9d904df6cb0dd3a449
rosebud-external-files <147299802+rosebud-external-files@users.noreply.github.com>
2023-10-11 00:43:03 -0400

    Update and rename html-codes-954187.js to html-codes-1954187.js

---
+++ html-codes-1954187.js
@@ -0,0 +1,28 @@
+{
+    "testimonials": [
+        {
+            "name": "Anonymous User",
+            "image": "https://uploads-ssl.webflow.com/64066f7ba948a8b9d8b1ca0e/651dbeb6b052a4c56ee59654_avatar-02.svg",
+            "content": "It felt good to be able to journal on my phone and to not have a person ask these questions that sometimes feel judgmental or invasive.",
+            "category":"general"
+        },
+        {
+            "name": "Davide Di Cillo",
+            "image": "https://uploads-ssl.webflow.com/64066f7ba948a8b9d8b1ca0e/6522e3a87ea9a737daf13c62_Davide-Di-Cillo-Avatar.png",
+            "content": "Rosebud is the perfect example of how AI can be used to improve people's life beyond the usual productivity enhancer. I'm really excited to see this out in the public, it's truly a game changer for personal growth.",
+            "category":"growth"
+        },
+        {
+            "name": "Anonymous User",
+            "image": "https://uploads-ssl.webflow.com/64066f7ba948a8b9d8b1ca0e/651dbeb6e503b7c1a8363125_avatar-07.svg",
+            "content": "Increase my productivity, staying away from people that hurt me, improving my mental health, and eventually telling someone I find trustworthy enough to comfort me in times of distress and self loathing.",
+            "category":"health,productivity"
+        },
+        {
+            "name": "Anonymous User",
+            "image": "https://uploads-ssl.webflow.com/64066f7ba948a8b9d8b1ca0e/651dbeb6e503b7c1a8363125_avatar-07.svg",
+            "content": "Increase my productivity, staying away from people that hurt me, improving my mental health, and eventually telling someone I find trustworthy enough to comfort me in times of distress and self loathing.",
+            "category":"health,productivity"
+        }
+    ]
+}
